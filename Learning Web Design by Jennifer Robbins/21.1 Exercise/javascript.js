8ea35abe7d2d5a32a4b1fcc777ff902a2c28f108
Coke <a.idzikowski@student.uw.edu.pl>
2020-10-22 17:07:19 +0200

    Upload exercises

---
+++ Learning Web Design by Jennifer Robbins/21.1 Exercise/javascript.js
@@ -0,0 +1,17 @@
+// 1
+var friends = ["a", "b", "c", "d"];
+
+alert(friends[2]);
+
+var name = "Daniel";
+
+if(name === "Jennifer")
+{
+    alert("That's my name too!");
+}
+
+var myVariable = 5;
+if(myVariable > 5)
+    alert("greater");
+else
+    alert("lower");
